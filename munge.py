1f38864b06a639b550fa5573671dc096358c316a
Peter Neve <p.neve.home@gmail.com>
2017-03-15 12:12:48 +0000

    Update for input and output to files.

--- munge.py
+++ munge.py
@@ -2,14 +2,29 @@
 import argparse
 __author__ = 'th3s3cr3tag3nt'
 
-parser = argparse.ArgumentParser(description='Word munger for password cracking by Th3 S3cr3t Ag3nt.')
-parser.add_argument('word', metavar='word',
+parser = argparse.ArgumentParser(
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+     description='''\
+ _ __ ___  _   _ _ __   __ _  ___   _ __  _   _ 
+| \'_ \' _ \| | | | \'_ \ / _\' |/ _ \ | \'_ \| | | |
+| | | | | | |_| | | | | (_| |  __/_| |_) | |_| |
+|_| |_| |_|\__,_|_| |_|\__, |\___(_) .__/ \__, |
+                       |___/       |_|    |___/ 
+
+Dirty little word munger by Th3 S3cr3t Ag3nt.
+'''
+                       )
+parser.add_argument('word', metavar='word', nargs='?',
                     help='word to munge')
+
 parser.add_argument('-l','--level', type=int, default=5,
                     help='munge level [0-9] (default 5)')
 
-parser.add_argument('--debug', dest='debug', action='store_const',
-                    const=1, default=0,)
+parser.add_argument('-i','--input', dest='input',
+                    help='input file')
+
+parser.add_argument('-o','--output', dest='output',
+                    help='output file')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -20,11 +35,6 @@ if args.level > 9:
 if args.level < 0:
 	args.level = 0;
 
-# Display what we have.
-if args.debug==1:
-	print ("Word: %s" % args.word )
-	print ("Level: %s" % args.level )
-
 # create word list
 wordlist = list()
 
@@ -32,43 +42,25 @@ def munge(wrd, level):
 	global wordlist
 	if level > 0:
 		wordlist.append( wrd )
-		#print wrd
-
-	if level > 0:
 		wordlist.append( wrd.upper() )
-
-	if level > 0:
 		wordlist.append( wrd.capitalize() )
 
 	if level > 2:
 		temp = wrd.capitalize()
 		wordlist.append( temp.swapcase() )
 
+	# Leet speak
 	if level > 4:
 		wordlist.append( wrd.replace('e', '3') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('a', '4') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('o', '0') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('i', '!') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('i', '1') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('l', '1') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('a', '@') )
-
-	if level > 4:
 		wordlist.append( wrd.replace('s', '$') )
 
-	# Leet speak
+	# Leet speak conmbinations
 	if level > 4:
 		temp = wrd
 		temp = temp.replace('e', '3')
@@ -142,31 +134,57 @@ def munge(wrd, level):
 		temp = temp.replace('s', '$')
 		wordlist.append( temp )
 
+def mungeword(wrd, level):
+	global wordlist
+	munge(wrd, level)
 
-wrd = args.word.lower()
+	if args.level > 6:
+		munge(wrd + '!', args.level)
+		munge(wrd + '.', args.level)
+		munge(wrd + '?', args.level)
+		munge(wrd + '_', args.level)
+		munge(wrd + '1', args.level)
 
-munge(wrd, args.level)
+	if args.level > 8:
+		munge(wrd + '0', args.level)
+		munge(wrd + '2', args.level)
+		munge(wrd + '3', args.level)
+		munge(wrd + '4', args.level)
+		munge(wrd + '5', args.level)
+		munge(wrd + '6', args.level)
+		munge(wrd + '7', args.level)
+		munge(wrd + '8', args.level)
+		munge(wrd + '9', args.level)
 
-if args.level > 6:
-	munge(wrd + '!', args.level)
-	munge(wrd + '.', args.level)
-	munge(wrd + '?', args.level)
-	munge(wrd + '_', args.level)
-	munge(wrd + '1', args.level)
-
-if args.level > 8:
-	munge(wrd + '0', args.level)
-	munge(wrd + '2', args.level)
-	munge(wrd + '3', args.level)
-	munge(wrd + '4', args.level)
-	munge(wrd + '5', args.level)
-	munge(wrd + '6', args.level)
-	munge(wrd + '7', args.level)
-	munge(wrd + '8', args.level)
-	munge(wrd + '9', args.level)
-
+if args.word:
+	wrd = args.word.lower()
+	mungeword(wrd. args.level)
+elif args.input:
+	## Open the file with read only permit
+	try:
+		with open(args.input) as f:
+			for wrd in f:
+				mungeword(wrd.rstrip(), args.level)
+			f.close()
+	except IOError:
+		print "Exiting\nCould not read file:", args.input
+else:
+	print "Nothing to do!!\nTry -h for help.\n";
 
 wordlist = set(wordlist)
 
-for word in wordlist:
-	print(word)
+
+if args.output:
+	## Open the file with read only permit
+	try:
+		with open(args.output, 'w') as f:
+			for word in wordlist:
+				#print(word)
+				f.write(word + "\n")
+			f.close()
+			print "Written to:", args.output
+	except IOError:
+		print "Exiting\nCould not write file:", args.input
+else:
+	for word in wordlist:
+		print(word)
