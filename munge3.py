c510104fbac9c822d09737af5f413953a17e68d6
code <code@>
2023-05-09 14:59:02 -0700

    added comments for basic description of functions and features

--- munge3.py
+++ munge3.py
@@ -12,7 +12,8 @@ parser = argparse.ArgumentParser(
 Dirty little word munger by Th3 S3cr3t Ag3nt.
 '''
 )
-
+#replaces chars with 'leet speak' alts
+#hands off nums to append() function
 def replace(w,chars,nums):
 	global wordlist
 	for c in chars:
@@ -20,19 +21,19 @@ def replace(w,chars,nums):
 			output = w.replace(c,chars[c])
 			wordlist.append(output)
 			append(output,nums)
-
+#appends passed in characters to words and adds them to the list
 def append(w,chars):
 	global wordlist
 	for c in chars:
 		wordlist.append(f'{w}{c}')
-
+#level 1 arg
 def basic(w):
 	global wordlist
 	wordlist.append(w)
 	wordlist.append(w.upper())
 	wordlist.append(w.capitalize())
 	wordlist.append(w.swapcase())
-
+#level 2 arg
 def advanced(w):
 	global wordlist
 	alts = {'e':'3','a':'4','i':'!','o':'0','s':'$'}
@@ -41,7 +42,7 @@ def advanced(w):
 	'8','9','10','11','13','3','4','5','6','7')
 	basic(w)
 	replace(w,alts,nums)
-
+#level 3 arg
 def expert(w):
 	global wordlist
 	alts = {'a':'@','i':'1','l':'1','s':'5'}
@@ -52,8 +53,7 @@ def expert(w):
 	'2011','86','87','89','90','91','92','93','94','95','98')
 	advanced(w)
 	replace(w, alts,nums)
-	
-
+#base switch case for 'level' arg
 def munge(word, level):
 	global wordlist
 	if 1 > level > 3:
@@ -66,17 +66,17 @@ def munge(word, level):
 			advanced(word)
 		case 3:
 			expert(word)
-
+#writes lines out to file
 def write_file():
 	with open(args.output, 'w') as f:
 		for word in wordlist:
 			f.writelines(f'{word}\n')
-
+#reads in lines and strips to normalize
 def read_file():
 	with open(args.input, 'r') as f:
 		for word in f:
 			munge(word.strip(), args.level)
-
+#loops to remove any '' strings
 def cleanup():
 	global wordlist
 	for c in wordlist:
@@ -101,10 +101,13 @@ if args.level < 0:
 
 #init empty word list
 wordlist = []
-
+#check if a list or single word was passed in
 if args.word:
+	#store word for later usage
 	name = args.word
+	#set to lower for normalization
 	word = args.word.lower()
+	#pass in word to munge 
 	munge(word, args.level)
 
 elif args.input:
@@ -113,23 +116,30 @@ elif args.input:
 	try:
 		read_file()
 	except IOError:
+		#display error based on read issue
 		print(f"Exiting\nCount not read file: {args.input}")
 else:
 	print("Nothing to do...\nTry -h for help.")
-
+#sort and make a set to eliminate dups
 wordlist = sorted(set(wordlist))
+#remove empty strings from list
 cleanup()
 
 if args.output:
 	#Open the file with write only
 	try:
 		write_file()
+		#display file name provide in output arg
 		print(f"Written to: {args.output}")
 	except IOError:
+		#display error based on write issue
 		print(f"Exiting\nCould not write file: {args.input}")
 else:
+	#get count of words generated
 	count = len(wordlist)
+	#output all words in console for copy/paste
 	for word in wordlist:
 		print(word)
+	#show number of words generated with word/file
 	print(f"Generated {count} words from {name}")
 
