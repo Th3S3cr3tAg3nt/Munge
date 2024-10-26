93b250f2050fdab59975ccd1716ea19901d7b39d
Peter Neve <th3s3cr3tag3nt@gmail.com>
2024-10-26 17:07:26 +0100

    Pythonic refactoring

--- munge.py
+++ munge.py
@@ -1,259 +1,128 @@
 #!/usr/bin/python3
 import argparse
+
 __author__ = 'th3s3cr3tag3nt'
 
+# Argument parser setup
 parser = argparse.ArgumentParser(
-	formatter_class=argparse.RawDescriptionHelpFormatter,
-     description='''\
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description='''\
  _ __ ___  _   _ _ __   __ _  ___   _ __  _   _
-| \'_ \' _ \| | | | \'_ \ / _\' |/ _ \ | \'_ \| | | |
+| '_ ' _ \\| | | | '_ \\ / _` |/ _ \\ | '_ \\| | | |
 | | | | | | |_| | | | | (_| |  __/_| |_) | |_| |
-|_| |_| |_|\__,_|_| |_|\__, |\___(_) .__/ \__, |
+|_| |_| |_|\\__,_|_| |_|\\__, |\\___(_) .__/ \\__, |
                        |___/       |_|    |___/
 
 Dirty little word munger by Th3 S3cr3t Ag3nt.
 '''
-                       )
-parser.add_argument('word', metavar='word', nargs='?',
-                    help='word to munge')
-
-parser.add_argument('-l','--level', type=int, default=5,
-                    help='munge level [0-9] (default 5)')
-
-parser.add_argument('-i','--input', dest='input',
-                    help='input file')
-
-parser.add_argument('-o','--output', dest='output',
-                    help='output file')
-
-# Parse the arguments
+)
+parser.add_argument('word', metavar='word', nargs='?', help='word to munge')
+parser.add_argument('-l', '--level', type=int, default=5, help='munge level [0-9] (default 5)')
+parser.add_argument('-i', '--input', help='input file')
+parser.add_argument('-o', '--output', help='output file')
 args = parser.parse_args()
 
-# Limit the level
-if args.level > 9:
-	args.level = 9
-if args.level < 0:
-	args.level = 0
+# Validate level argument
+args.level = max(0, min(args.level, 9))
 
-# create word list
-wordlist = list()
+# Helper function for leet transformations
+def leet_transform(word, patterns):
+    for pattern in patterns:
+        yield ''.join(pattern.get(char, char) for char in word)
 
-def munge(wrd, level):
-	global wordlist
-	if level > 0:
-		wordlist.append( wrd )
-		wordlist.append( wrd.upper() )
-		wordlist.append( wrd.capitalize() )
+# Define transformation patterns for various levels
+leet_patterns = [
+    {},  # No transformation
+    {'e': '3', 'a': '4', 'o': '0', 'i': '1', 'l': '1', 's': '$'},  # Leet set 1
+    {'e': '3', 'a': '@', 'o': '0', 'i': '1', 'l': '1', 's': '$'},  # Leet set 2
+    {'e': '3', 'a': '4', 'o': '0', 'i': '!', 'l': '1', 's': '$'},  # Leet set 3
+    {'e': '3', 'a': '@', 'o': '0', 'i': '!', 'l': '1', 's': '$'},  # Leet set 4
+    {'e': '3', 'a': '4', 'o': '0', 'i': '1', 'l': '1', 's': '5'},  # Leet set 5
+    {'e': '3', 'a': '@', 'o': '0', 'i': '1', 'l': '1', 's': '5'},  # Leet set 6
+    {'e': '3', 'a': '4', 'o': '0', 'i': '!', 'l': '1', 's': '5'},  # Leet set 7
+    {'e': '3', 'a': '@', 'o': '0', 'i': '!', 'l': '1', 's': '5'},  # Leet set 8
+]
 
-	if level > 2:
-		temp = wrd.capitalize()
-		wordlist.append( temp.swapcase() )
+# Generate capitalization variants
+def capitalize_variants(word):
+    """Generate different capitalization forms for a word."""
+    return {word, word.upper(), word.capitalize(), word.lower()}
 
-	# Leet speak
-	if level > 4:
-		wordlist.append( wrd.replace('e', '3') )
-		wordlist.append( wrd.replace('a', '4') )
-		wordlist.append( wrd.replace('o', '0') )
-		wordlist.append( wrd.replace('i', '!') )
-		wordlist.append( wrd.replace('i', '1') )
-		wordlist.append( wrd.replace('l', '1') )
-		wordlist.append( wrd.replace('a', '@') )
-		wordlist.append( wrd.replace('s', '$') )
+# Generate transformations based on level
+def munge(word, level):
+    transformations = set()
+    for variant in capitalize_variants(word):
+        transformations.add(variant)
+        if level > 4:
+            transformations |= set(leet_transform(variant, leet_patterns[:level - 4]))
+    return transformations
 
-	# Leet speak conmbinations
-	if level > 4:
-		temp = wrd
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '4')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('i', '1')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
+# Apply transformations and append suffixes
+def munge_with_suffixes(word, level):
+    suffixes = []
+    if level > 4:
+        suffixes.extend(['1', '123456', '12', '2', '123', '!', '.', '2'])
+    if level > 6:
+        suffixes.extend([
+            '?', '_', '0', '01', '69', '23', '24', '25', '1234', '8', '9', '10', 
+            '11', '13', '3', '4', '5', '6', '7'
+        ])
+    if level > 7:
+        suffixes.extend([
+            '07', '08', '09', '14', '15', '16', '17', '18', '19', '21', '22', '20', 
+            '77', '88', '99', '12345', '123456789'
+        ])
+    if level > 8:
+        suffixes.extend([
+            '00', '02', '03', '04', '05', '06', '19', '20', '25', '26', '27', 
+            '28', '007', '1234567', '12345678', '111111', '111', '777', '666', 
+            '101', '33', '44', '55', '66', '2022', '2023', '2024', '2025', '86', 
+            '2021', '2020', '2019', '87', '89', '90', '91', '92', '93', '94'
+            '95', '98'
+        ])
 
-	if level > 4:
-		temp = wrd
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '@')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('i', '1')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
+    munged_words = munge(word, level)
+    for suffix in suffixes:
+        for variant in capitalize_variants(word):
+            munged_words |= munge(variant + suffix, level)
+    return munged_words
 
-	if level > 4:
-		temp = wrd
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '4')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('i', '!')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
+# Generate munged words from input
+def generate_wordlist(word_list, level):
+    result = set()
+    for word in word_list:
+        word = word.strip().lower()
+        result |= munge_with_suffixes(word, level)
+    return result
 
-	if level > 4:
-		temp = wrd
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '4')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('l', '1')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
+# Main logic for handling input/output
+def main():
+    word_list = []
+    if args.word:
+        word_list.append(args.word)
+    elif args.input:
+        try:
+            with open(args.input) as f:
+                word_list = f.readlines()
+        except IOError:
+            print(f"Exiting\nCould not read file: {args.input}")
+            return
+    else:
+        print("Nothing to do!!\nTry -h for help.\n")
+        return
 
-	# Capitalize
-	if level > 4:
-		temp = wrd.capitalize()
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '4')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('i', '1')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
+    wordlist = generate_wordlist(word_list, args.level)
+    
+    if args.output:
+        try:
+            with open(args.output, 'w') as f:
+                f.write('\n'.join(wordlist) + '\n')
+            print(f"Written to: {args.output}")
+        except IOError:
+            print(f"Exiting\nCould not write file: {args.output}")
+    else:
+        for word in wordlist:
+            print(word)
 
-	if level > 4:
-		temp = wrd.capitalize()
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '@')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('i', '1')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
-
-	if level > 4:
-		temp = wrd.capitalize()
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '4')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('i', '!')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
-
-	if level > 4:
-		temp = wrd.capitalize()
-		temp = temp.replace('e', '3')
-		temp = temp.replace('a', '4')
-		temp = temp.replace('o', '0')
-		temp = temp.replace('l', '1')
-		temp = temp.replace('s', '$')
-		wordlist.append( temp )
-
-def mungeword(wrd, level):
-	global wordlist
-	munge(wrd, level)
-
-	if args.level > 4:
-		munge(wrd + '1', args.level)
-		munge(wrd + '123456', args.level)
-		munge(wrd + '12', args.level)
-		munge(wrd + '2', args.level)
-		munge(wrd + '123', args.level)
-		munge(wrd + '!', args.level)
-		munge(wrd + '.', args.level)
-
-	if args.level > 6:
-		munge(wrd + '?', args.level)
-		munge(wrd + '_', args.level)
-		munge(wrd + '0', args.level)
-		munge(wrd + '01', args.level)
-		munge(wrd + '69', args.level)
-		munge(wrd + '21', args.level)
-		munge(wrd + '22', args.level)
-		munge(wrd + '23', args.level)
-		munge(wrd + '1234', args.level)
-		munge(wrd + '8', args.level)
-		munge(wrd + '9', args.level)
-		munge(wrd + '10', args.level)
-		munge(wrd + '11', args.level)
-		munge(wrd + '13', args.level)
-		munge(wrd + '3', args.level)
-		munge(wrd + '4', args.level)
-		munge(wrd + '5', args.level)
-		munge(wrd + '6', args.level)
-		munge(wrd + '7', args.level)
-
-	if args.level > 7:
-		munge(wrd + '07', args.level)
-		munge(wrd + '08', args.level)
-		munge(wrd + '09', args.level)
-		munge(wrd + '14', args.level)
-		munge(wrd + '15', args.level)
-		munge(wrd + '16', args.level)
-		munge(wrd + '17', args.level)
-		munge(wrd + '18', args.level)
-		munge(wrd + '19', args.level)
-		munge(wrd + '24', args.level)
-		munge(wrd + '77', args.level)
-		munge(wrd + '88', args.level)
-		munge(wrd + '99', args.level)
-		munge(wrd + '12345', args.level)
-		munge(wrd + '123456789', args.level)
-
-	if args.level > 8:
-		munge(wrd + '00', args.level)
-		munge(wrd + '02', args.level)
-		munge(wrd + '03', args.level)
-		munge(wrd + '04', args.level)
-		munge(wrd + '05', args.level)
-		munge(wrd + '06', args.level)
-		munge(wrd + '19', args.level)
-		munge(wrd + '20', args.level)
-		munge(wrd + '25', args.level)
-		munge(wrd + '26', args.level)
-		munge(wrd + '27', args.level)
-		munge(wrd + '28', args.level)
-		munge(wrd + '007', args.level)
-		munge(wrd + '1234567', args.level)
-		munge(wrd + '12345678', args.level)
-		munge(wrd + '111111', args.level)
-		munge(wrd + '111', args.level)
-		munge(wrd + '777', args.level)
-		munge(wrd + '666', args.level)
-		munge(wrd + '101', args.level)
-		munge(wrd + '33', args.level)
-		munge(wrd + '44', args.level)
-		munge(wrd + '55', args.level)
-		munge(wrd + '66', args.level)
-		munge(wrd + '2008', args.level)
-		munge(wrd + '2009', args.level)
-		munge(wrd + '2010', args.level)
-		munge(wrd + '2011', args.level)
-		munge(wrd + '86', args.level)
-		munge(wrd + '87', args.level)
-		munge(wrd + '89', args.level)
-		munge(wrd + '90', args.level)
-		munge(wrd + '91', args.level)
-		munge(wrd + '92', args.level)
-		munge(wrd + '93', args.level)
-		munge(wrd + '94', args.level)
-		munge(wrd + '95', args.level)
-		munge(wrd + '98', args.level)
-
-if args.word:
-	wrd = args.word.lower()
-	mungeword(wrd, args.level)
-elif args.input:
-	## Open the file with read only permit
-	try:
-		with open(args.input) as f:
-			for wrd in f:
-				mungeword(wrd.rstrip(), args.level)
-			f.close()
-	except IOError:
-		print ("Exiting\nCould not read file:", args.input)
-else:
-	print ("Nothing to do!!\nTry -h for help.\n")
-
-wordlist = set(wordlist)
-
-
-if args.output:
-	## Open the file with read only permit
-	try:
-		with open(args.output, 'w') as f:
-			for word in wordlist:
-				#print(word)
-				f.write(word + "\n")
-			f.close()
-			print ("Written to:", args.output)
-	except IOError:
-		print ("Exiting\nCould not write file:", args.input)
-else:
-	for word in wordlist:
-		print(word)
+if __name__ == "__main__":
+    main()
